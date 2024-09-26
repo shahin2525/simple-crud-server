0669100ca839b84e707e752e8093facf4ba4380e
shahin2525 <shahin451999@gmail.com>
2024-09-26 13:31:37 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 5000;
+//
+
+const { MongoClient, ServerApiVersion } = require("mongodb");
+const uri =
+  "mongodb+srv://simple-crud-server:RXhWyhAvzDa2WxCv@cluster0.ax6qyiu.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log(
+      "Pinged your deployment. You successfully connected to MongoDB!"
+    );
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+//
+app.get("/", (req, res) => {
+  res.send("Hello World!");
+});
+// simple-crud-server
+// RXhWyhAvzDa2WxCv
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`);
+});
